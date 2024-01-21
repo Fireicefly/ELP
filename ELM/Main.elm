9ca5cbfa796ebfbc66ccd3fc27dc1c9732cf9161
Alphavdy <remy.vaudey@insa-lyon.fr>
2024-01-21 01:29:19 +0100

    Récupération des infos triées

--- ELM/Main.elm
+++ ELM/Main.elm
@@ -3,47 +3,76 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (style)
-import Html.Events exposing (..)
+import Html.Events exposing (onClick)
 import Http
-import Json.Decode exposing (Decoder, map4, field, int, string)
+import Json.Decode as JD exposing (..) 
 
 -- MODEL
 
 type Model
-    = Loading
-    | Loaded String
+    = 
+    Start
+    | Loading    
+    | Loaded_def Definition
     | Failed
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  (Loading, fetchData)
+  (Start, Cmd.none)
 
 -- UPDATE
 
 type Msg
-    = GotDef(Result Http.Error String)
+    = 
+    GetDef 
+    | GotDef(Result Http.Error Definition)
 
 update :  Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
+        
+        GetDef -> 
+            (Loading, fetchData)
         GotDef (Ok data) ->
-            (Loaded data, Cmd.none)
+            (Loaded_def data, Cmd.none)
         GotDef (Err _) ->
             (Failed, Cmd.none)
 
 -- VIEW
 
 view : Model -> Html Msg
-view model =
+view model = 
+    div [] [
+      h1 [][text "Devinette"]
+      , button [onClick GetDef][text "Reload"]
+      ,findDef model
+    ]
+    
+    
+
+findDef model = 
     case model of
-        Loading ->
-            div [] [ text "Chargement..." ]
+            Start ->
+                div [] [text "Bienvenue"]
+            Loading ->
+                div [] [ text "Chargement..." ]
 
-        Loaded definition ->
-            div [] [ text definition ]
+            
 
-        Failed ->
-            div [] [ text "Échec du chargement" ]
+
+            Loaded_def definitions->                
+                
+                div [] [text (definitions.word)]
+                
+                    
+
+
+            Failed ->
+                div [] [ text "Échec du chargement" ]
+
+--SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
 
 -- MAIN
 
@@ -52,14 +81,95 @@ main =
         { init = init
         , update = update
         , view = view
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
 
+type alias Tableau = List Definition
+
+
+--Fonctions pour Decoder le site
+
+type alias Definition =
+    { word : String    
+    ,meanings : List Meaning     
+    }
+
+type alias Meaning =
+    { 
+    definitions : List SubDefinition
+    --, synonyms : List String
+    --, antonyms : List String
+    }
+
+type alias SubDefinition =
+    { definition : String
+    --, example : Maybe String
+    --, synonyms : List String
+    --, antonyms : List String
+    }
+
+
+--removeFirstAndLastCharacter : String -> Msg
+--removeFirstAndLastCharacter jsonString =
+    --let
+        
+        --slicedString = String.dropRight 1 (String.dropLeft 1 jsonString)
+    
+    
+    --in GotDef (jsonString)           
+            
+decodeArray : Decoder Definition
+decodeArray  =
+    list decodeDefinition
+      |> andThen (\definitions ->
+            case definitions of
+                [] ->
+                    -- Gérer le cas où la liste est vide
+                    JD.fail "Empty list"
+
+                firstElement :: _ ->
+                    -- Récupérer le premier élément
+                    JD.succeed firstElement
+        )
+
+decodeDefinition : Decoder Definition
+decodeDefinition  =
+    JD.map2 Definition
+        (JD.field "word" JD.string)       
+        (JD.field "meanings" (JD.list decodeMeaning))
+               
+
+
+
+decodeMeaning : Decoder Meaning
+decodeMeaning =
+    JD.map Meaning        
+        (JD.field "definitions" (JD.list decodeSubDefinition))
+        --(Decode.field "synonyms" (Decode.list Decode.string))
+        --(Decode.field "antonyms" (Decode.list Decode.string))
+
+decodeSubDefinition : Decoder SubDefinition
+decodeSubDefinition =
+    JD.map SubDefinition
+        (JD.field "definition" JD.string)
+        --(Decode.field "example" (Decode.maybe Decode.string))
+        --(Decode.field "synonyms" (Decode.list Decode.string))
+        --(Decode.field "antonyms" (Decode.list Decode.string))
+
+
+--decodeDefinition =
+    --JD.map Definition
+    --(JD.field "meanings" JD.string (JD.list (JD.field "definitions" (JD.list (JD.field "definition" JD.string)))))
+    
+
+--decodeMeanings = 
+    --JD.list decodeDefinition
 
 -- HTTP
 fetchData : Cmd Msg
 fetchData =
     Http.get
         { url = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
-        , expect = Http.expectString GotDef 
+        , expect = Http.expectJson GotDef decodeArray
         }
+
